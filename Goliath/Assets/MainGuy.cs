59d71f09c393bee03a62304675d8b717994f6ac7
johansergeyssels <johan@johans-MacBook-Pro.local>
2015-02-08 16:10:42 +0100

    first attempt to protoype
    first attempt prototyping

---
+++ Goliath/Assets/MainGuy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGuy : MonoBehaviour
+{
+	public float jumpforce = 0;
+	public float speed = 1f;
+
+	private bool air = false;
+
+	void Start ()
+	{
+
+	}
+
+	void Update ()
+	{
+		Vector3 moveVector = new Vector3 (Input.GetAxis ("Horizontal") * speed, 0, 0);
+		rigidbody.AddForce (moveVector);
+		if (Input.GetButtonDown ("Jump") && !air) {
+			rigidbody.AddForce(new Vector3(0, jumpforce, 0));
+			air = true;
+		}
+		Debug.Log (rigidbody.velocity);
+	}
+
+	void OnCollisionEnter(Collision col){
+		air = false;
+	}
+}
