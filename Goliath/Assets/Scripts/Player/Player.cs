87b388c1d939be82c7c0bdf563eddbc8528be6b8
johansergeyssels <johan@johans-MacBook-Pro.local>
2015-03-08 18:45:01 +0100

    prototyping another way
    nieuwe manier + refactoring 2D scroller basics

---
+++ Goliath/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(PlayerPhysics))]
+public class Player : MonoBehaviour {
+	public float jumpforce = 0;
+	public float acc = 1f;
+	public float maxSpeed = 10;
+	
+	private PlayerController controller;
+	private PlayerPhysics physics;
+
+	void Awake () {
+		physics = GetComponent<PlayerPhysics>();
+		controller = GetComponent<PlayerController>();
+		
+		physics.player = this;
+		
+		controller.physics = physics;
+	}
+
+	void Update () {
+		
+	}
+}
---
+++ Goliath/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	[HideInInspector]
+	public PlayerPhysics physics;
+	
+	private bool jump = false;
+
+	void Update () {
+		if (Input.GetButtonDown("Jump")) {
+			jump = true;
+		}
+	}
+	
+	void FixedUpdate() { 
+		float horizontal = Input.GetAxisRaw ("Horizontal");
+		physics.Move(horizontal, jump);
+		jump = false;
+	}
+}
