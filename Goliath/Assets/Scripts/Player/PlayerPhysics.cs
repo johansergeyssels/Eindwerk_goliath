5b29c6606b61bfde90ea5ac004a32cea1210595a
johansergeyssels <johan@johans-MacBook-Pro.local>
2015-03-16 23:18:55 +0100

    prototyping: walljumps

--- Goliath/Assets/Scripts/Player/PlayerPhysics.cs
+++ Goliath/Assets/Scripts/Player/PlayerPhysics.cs
@@ -12,12 +12,15 @@ public class PlayerPhysics : MonoBehaviour {
 	
 	private Rigidbody rbody;
 	private List<GameObject> colliderGameObjects = new List<GameObject>();
-	private Vector3 groundCheckVector;
+	private Vector3 groundCheckVector, leftWallCheckVector, rightWallCheckVector, ceilingCheckVector;
 
 	void Awake () {
 		rbody = gameObject.AddComponent<Rigidbody>();
 		rbody.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
 		groundCheckVector = Vector3.down * 0.5f; //around the base of the feets
+		leftWallCheckVector = Vector3.left * 0.5f;
+		rightWallCheckVector = Vector3.right * 0.5f;
+		ceilingCheckVector = Vector3.up * 0.5f;
 	}
 	
 	public void Update() {
@@ -25,27 +28,57 @@ public class PlayerPhysics : MonoBehaviour {
 	}
 	
 	public void Move(float horizontal, bool jump) {
-		CheckOnGround();
+		onGround = CheckOnCollision(groundCheckVector);
+		onLeft = CheckOnCollision(leftWallCheckVector);
+		onRight = CheckOnCollision(rightWallCheckVector);
+		onCeiling = CheckOnCollision(ceilingCheckVector);
+		
+		if(onLeft) {
+			Debug.Log("left " + Time.frameCount);
+		}
+		
+		if(onRight) {
+			Debug.Log("right " + Time.frameCount);
+		}
+		
+		if(horizontal == 0 && onGround && !jump) {
+			if(rigidbody.drag < 100) {
+				rigidbody.drag++;
+			}
+		}
+		else {
+			rigidbody.drag = 0;
+		}
 		
 		Vector3 moveVector = Vector3.zero;
-		if(rigidbody.velocity.x < player.maxSpeed) {
+		if(Mathf.Abs(rigidbody.velocity.x) < player.maxSpeed) {
 			moveVector.x = horizontal * player.acc;
 		}
 		
-		if(onGround && jump) {
-			moveVector.y = player.jumpforce;
+		if(jump) {
+			if(onGround) {
+				moveVector.y = player.jumpforce;
+			}
+			else if(onLeft) {
+				moveVector.x = player.jumpforce;
+				moveVector.y = player.jumpforce;
+			}
+			else if(onRight) {
+				moveVector.x = -player.jumpforce;
+				moveVector.y = player.jumpforce;
+			}
 		}
 		
 		rigidbody.AddForce (moveVector);
 	}
 	
-	private void CheckOnGround() {
-		onGround = false;
-		Collider[] colliders = Physics.OverlapSphere(transform.TransformPoint(groundCheckVector), 0.3f);
+	private bool CheckOnCollision(Vector3 v) {
+		Collider[] colliders = Physics.OverlapSphere(transform.TransformPoint(v), 0.3f);
 		for (int i = 0; i < colliders.Length; i++)
 		{
 			if (colliders[i].gameObject != gameObject)
-				onGround = true;
+				return true;
 		}
+		return false;
 	}
 }
