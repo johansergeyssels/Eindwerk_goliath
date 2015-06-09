7533d802bb20a109d484bd64e26e92b1db519278
johan <johan@johans-MacBook-Pro.local>
2015-06-09 10:50:30 +0200

    boundries

---
+++ Goliath/Assets/Scripts/Game/CameraBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+	private float maxX, minX, maxY, minY;
+	
+	void Start () {
+		if(Game.current == null) return;
+		var orthosize = GetComponent<Camera>().orthographicSize;
+		var sizeX = orthosize * (float)Screen.width / (float)Screen.height;
+		var sizeY = orthosize;
+		maxX = Game.current.maxX - sizeX;
+		minX = Game.current.minX + sizeX;
+		maxY = Game.current.maxY - sizeY;
+		minY = Game.current.minY + sizeY;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Game.current == null) return;
+		
+		var movement = transform.position;
+		if(maxX > Game.current.player.transform.position.x &&
+		   minX < Game.current.player.transform.position.x) {
+			movement.x = Game.current.player.transform.position.x;
+		}
+		if(maxY > Game.current.player.transform.position.y &&
+		   minY < Game.current.player.transform.position.y) {
+			movement.y = Game.current.player.transform.position.y;  
+		}
+		transform.position = movement;
+	}
+}
---
+++ Goliath/Assets/Scripts/Game/RandomPlatformGenerator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomPlatformGenerator : MonoBehaviour {
+	[SerializeField]
+	private MovingPlatformBehaviour platform;
+	[SerializeField]
+	private int maximumAmount = 20;
+	
+	private float timer = 0;
+	private Vector3 cohesion;
+	private float cohesionRadius = 10;
+	private Collider[] platforms;
+	
+	void Start () {
+		for (int i = 0; i < maximumAmount; i++) {
+			var newPlatform = Instantiate<MovingPlatformBehaviour>(platform);
+			var x = Random.Range(Game.current.minX, Game.current.maxX);
+			var y = Random.Range(Game.current.minY, Game.current.maxY);
+			newPlatform.transform.position = new Vector3(x, y, 0);
+			newPlatform.transform.localScale = new Vector3(Random.Range(3, 10), 1, 0);
+			newPlatform.movement = new Vector3(Random.Range(-0.01f, 0.01f), Random.Range(-0.01f, 0.01f), 0);
+		}
+	}
+}
